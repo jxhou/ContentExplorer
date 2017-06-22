60a1c078ba391f6911b0a82b76eca44d9a71131f
jhou <jhou@opentext.com>
2017-06-22 06:57:18 -0400

    Add material support along with animation.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+// in order to animation of material components, or use animation in your own components
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,  // enable animation functionality of some material components
   ],
   providers: [],
   bootstrap: [AppComponent]
