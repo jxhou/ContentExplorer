afec0495d594fa1ca1ae5c114cdddf103dbac2f7
jhou <jhou@opentext.com>
2017-07-06 08:35:17 -0400

    control theme from app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  host: {
+    '[class]' : 'theme',
+  }
 })
 export class AppComponent {
   title = 'app';
+  theme = 'pink-theme';
 }
