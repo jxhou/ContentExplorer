03890958a90e52df9a1f8f71049b258682875635
jhou <jhou@opentext.com>
2017-09-01 08:48:31 -0400

    ngrx support continue

--- src/app/reducers/theme.ts
+++ src/app/reducers/theme.ts
@@ -8,3 +8,17 @@ export interface State {
 const initialState: State = {
   themeName: 'indigo-theme'
 };
+
+export function reducer(state = initialState, action: theme.Actions): State {
+  switch (action.type) {
+    case theme.SET_THEME:
+      return {
+        themeName: action.payload,
+      };
+
+    default:
+      return state;
+  }
+}
+
+export const getThemeName = (state: State) => state.themeName;
