3209b166dc00323acf152be01589a69b47c833b6
jhou <jhou@opentext.com>
2018-04-02 07:35:40 -0400

    settings route

--- src/app/settings/settings.module.ts
+++ src/app/settings/settings.module.ts
@@ -1,10 +1,12 @@
-import { NgModule, ModuleWithProviders } from '@angular/core';
+import { NgModule, ModuleWithProviders, Type, ANALYZE_FOR_ENTRY_COMPONENTS, InjectionToken } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, provideRoutes } from '@angular/router';
 import { RouterModule } from '@angular/router';
 
 import { SharedModule } from '@app/shared';
 import { SettingsComponent } from './settings.component';
+
+export const SETTINGS_ROUTES = new InjectionToken<Routes>('settings.routes');
 
 @NgModule({
   declarations: [SettingsComponent],
@@ -24,6 +26,22 @@ export class SettingsModule {
         providers: [
           provideRoutes(routes)
         ]
+    }
+  }
+
+  static withSettingRoutes(routes: Routes) {
+    return {
+      ngModule: SettingsModule,
+      providers: [
+        {
+          provide: ANALYZE_FOR_ENTRY_COMPONENTS,
+          useValue: routes, multi: true
+        },
+        {
+          provide: SETTINGS_ROUTES,
+          useValue: routes, multi: true
+        },
+      ]
     }
   }
 
