f1452e6c1fd98d4262f4863a10a6d66703b1f81f
jhou <jhou@opentext.com>
2020-02-05 21:49:26 -0500

    material update

--- src/app/shared/shared-material.module.ts
+++ src/app/shared/shared-material.module.ts
@@ -1,14 +1,23 @@
 // This is the shared module including all the material components used in current projects.
 // More components should be added, once it is needed.
 import { NgModule } from '@angular/core';
-import 'hammerjs';
-import { MatInputModule, MatButtonModule, MatButtonToggleModule,
-         MatCheckboxModule, MatRadioModule,
-         MatSelectModule, MatAutocompleteModule,
-         MatCardModule, MatListModule, MatTabsModule,
-         MatMenuModule, MatSidenavModule, MatToolbarModule,
-         MatIconModule, MatChipsModule,
-         MatDialogModule, MatSnackBarModule, } from '@angular/material';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   imports: [
