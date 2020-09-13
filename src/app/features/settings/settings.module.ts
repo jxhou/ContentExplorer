00df0599545ea7244f6ed27d38fa136e88fe4835
jhou <jhou@opentext.com>
2020-09-12 21:50:46 -0400

    Angular 10.1 update

--- src/app/features/settings/settings.module.ts
+++ src/app/features/settings/settings.module.ts
@@ -25,7 +25,7 @@ export class SettingsModule {
   // provideRoutes() will declare entryComponents and add route configuration.
   // However this relies on the passed in routes with correct route path to target this setting page 
   // as its container. See more in src\app\routing\routing.module.ts. Option 2 solves this problem.
-  static withRoutes(routes: Routes): ModuleWithProviders {
+  static withRoutes(routes: Routes): ModuleWithProviders<SettingsModule> {
     return {
         ngModule: SettingsModule,
         providers: [
@@ -40,21 +40,27 @@ export class SettingsModule {
   // as a dependency, and will be dynamically added as child routes using router api.
   // The difference here from Option 1 is that we dynamically add child routes ourself instead of 
   // registering routes using router's default ROUTES token.
-  static withSettingRoutes(routes: Routes) {
+  // Option 2: a better way to register its child setting page.
+// Here we use ANALYZE_FOR_ENTRY_COMPONENTS to register entryComponents, and use a custom token 
+// SETTINGS_ROUTES to provide setting page components, which will be injected into SettingsComponent
+// as a dependency, and will be dynamically added as child routes using router api.
+// The difference here from Option 1 is that we dynamically add child routes ourself instead of 
+// registering routes using router's default ROUTES token.
+static withSettingRoutes(routes: Routes): ModuleWithProviders<SettingsModule> {
     return {
-      ngModule: SettingsModule,
-      providers: [
-        {
-          provide: ANALYZE_FOR_ENTRY_COMPONENTS,
-          useValue: routes, multi: true
-        },
-        {
-          provide: SETTINGS_ROUTES,
-          useValue: routes, multi: true
-        },
-      ]
-    }
-  }
+        ngModule: SettingsModule,
+        providers: [
+            {
+                provide: ANALYZE_FOR_ENTRY_COMPONENTS,
+                useValue: routes, multi: true
+            },
+            {
+                provide: SETTINGS_ROUTES,
+                useValue: routes, multi: true
+            },
+        ]
+    };
+}
 
   constructor() {
   }
