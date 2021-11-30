796c16849f45ef89f50620235234a82c550168a1
Jianxin Hou <jianxin@rideamigos.com>
2021-11-29 21:44:14 -0500

    update to angular 13

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms'
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { SharedMaterialModule } from './shared-material.module';
-import { FlexLayoutModule } from '@angular/flex-layout';
+// import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
 import { LayoutModule } from './layout/layout.module';
 import { AlertComponent } from './alert/alert.component';
@@ -27,7 +27,7 @@ import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.compo
     SharedMaterialModule,
 
     // Flex
-    FlexLayoutModule,
+    // FlexLayoutModule,
 
     LayoutModule
   ],
@@ -35,7 +35,7 @@ import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.compo
   ],
   exports:  [
     CommonModule, ReactiveFormsModule, FormsModule,
-    FlexLayoutModule,
+    // FlexLayoutModule,
     SharedMaterialModule, LayoutModule,
     AlertComponent, PlaceholderDirective, LoadingSpinnerComponent
    ]
